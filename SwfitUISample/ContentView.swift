2c3ed62947f96c9243b902c5b609118df5de6960
ouke <ou.ke.dev@gmail.com>
2024-04-21 10:15:31 +0900

    init

---
+++ SwfitUISample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwfitUISample
+//
+//  Created by Wang Jie on 2024/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwfitUISample/SwfitUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwfitUISampleApp.swift
+//  SwfitUISample
+//
+//  Created by Wang Jie on 2024/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwfitUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
